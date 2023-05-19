99dcf20aa4ffe1f7f2b396c867807e3584159050
Vlad Dumitru <vlad-dumitru@outlook.com>
2023-05-19 18:02:37 +0300

    Creare schemă bază de date

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,15 +5,26 @@ import { SessionProvider } from "next-auth/react";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <Head>
+        <meta
+          name="description"
+          content="Realizat de Dumitru A. Vlad, Grupa 302"
+        />
+      </Head>
+      <SessionProvider session={session}>
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </SessionProvider>
+    </>
   );
 };
 
